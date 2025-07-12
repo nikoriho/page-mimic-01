3717ffc5ed831cb9618ed7522f8a43c751674961
nikoriho <nigonoka@gmail.com>
2025-07-12 23:46:38 +0900

    feat: add reponsive behavior for search button and final overall adjustment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import "./styles/main.scss";
 import { initScrollBtn } from "./js/scrollUp";
 import { initDropdown } from "./js/dropdown";
+import { initToggleSearch } from "./js/toggleSearch";
 
 initScrollBtn();
 initDropdown();
+initToggleSearch();
