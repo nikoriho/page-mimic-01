61bcab641f18ecf5e49814fbf512a1845e5220dc
nikoriho <nigonoka@gmail.com>
2025-07-18 06:32:22 +0900

    refactor: move and declare all routes to top of the router.js

--- src/js/router.js
+++ src/js/router.js
@@ -1,5 +1,11 @@
 export { initRouter };
 
+const routes = {
+    "/":        () => import("./views/home.js"),
+    "/news":    () => import("./views/news.js"),
+    "/recruit": () => import("./views/recruit.js"),
+}
+
 function initRouter() {
     // Initial render
     render(location.pathname);
@@ -7,7 +13,7 @@ function initRouter() {
     // Intercept link clicks
     document.body.addEventListener("click", (e) => {
         const link = e.target.closest("a");
-        if (link && link.getAttribute("href").startWith("/") && !link.hasAttribute("target")) {
+        if (link && link.getAttribute("href")?.startsWith("/") && !link.hasAttribute("target")) {
             e.preventDefault();
             navigate(link.getAttribute("href"));
         }
@@ -22,18 +28,10 @@ function initRouter() {
 async function render(path) {
     let viewModule;
 
-    switch (path) {
-        case "/":
-            viewModule = await import("./views/home.js");
-            break;
-        case "/news":
-            viewModule = await import("./views/news.js");
-            break;
-        case "/recruit":
-            viewModule = await import("./views/recruit.js");
-            break;
-        default:
-            viewModule = await import("./views/notfound.js");
+    if (Object.hasOwn(routes, path)) {
+        viewModule = await routes[path]();
+    } else {
+        viewModule = await import("./views/notfound.js");
     }
 
     const view = viewModule.default;
